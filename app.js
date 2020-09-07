78138f7fa7c289ea596db10c94b89e1f5db80780
Valentin Popescu <valentinpopescu64@gmail.com>
2020-09-07 15:55:43 +0300

    changed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/uploads", express.static("uploads"));
 /* Database */
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://valentin:<password>@cluster0-fsimv.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://valentin:abcd1234@cluster0-fsimv.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 console.log(`uri mongodb ${process.env.MONGODB_URI}`);
 
 const user_api = require("./api/routes/user_api").route;
